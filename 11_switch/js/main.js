b56f869a47707c96c42bdfbf704efc8794d7b52d
Simone Giusti <info@simonegiusti.it>
2022-07-05 10:48:24 +0200

    aggiunto esempio switch

---
+++ 11_switch/js/main.js
@@ -0,0 +1,36 @@
+let voto = 6;
+
+switch (voto) {
+    case 1:
+        alert('Non hai studiato');
+        break;
+    case 2: 
+        alert('Meglio non raccontarlo a casa');
+        break;
+    case 3:
+        alert('Molto male');
+        break;
+    case 4:
+        alert('Si può far meglio');
+        break;
+    case 5:
+        alert('Ci siamo quasi');
+        break;
+    case 6:
+        alert('La salvezza');
+        break;
+    case 7:    
+        alert('Molto bene');
+        break;
+    case 8:
+        alert('Il secchione');
+        break;
+    case 9:
+        alert('Meglio è difficile');
+        break;
+    case 10:
+        alert('Meglio del prof');
+        break;
+    default: 
+        alert('Voto non classificabile');
+}
